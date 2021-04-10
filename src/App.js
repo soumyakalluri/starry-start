547db858fa15dfa77a655e41cb476d334dca75ce
Soumya Kalluri <soumya.kalluri11@gmail.com>
2021-04-10 03:26:17 -0700

    completed firebase login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import React, {Component} from 'react';
+import Login from './components/Login';
 
-function App() {
+firebase.initializeApp({
+  apiKey: "AIzaSyBD1ZZ-FzcVuz2cqGpxH91OXMtZXLPV-Fw",
+  authDomain: "starry-start.firebaseapp.com"
+})
+
+class App extends Component {
+  state = {
+    isSignedIn: false
+  }
+
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn: !!user});
+    });
+
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.isSignedIn ? (
+      <div>
+        <h1>start</h1>
+        <button onClick={() => firebase.auth().signOut()}>log out</button>
+      </div>
+      ) : (
+      <div className="body">
+        <h1>sign in to start</h1>
+        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+      </div>
+    )}
     </div>
   );
+
+  }
 }
 
 export default App;
