d9d834130a2cf5311d693ee7c3e869c0b1c0665d
Soumya Kalluri <soumya.kalluri11@gmail.com>
2021-04-10 18:01:02 -0700

    added moon and space popup

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import React, {Component} from 'react';
 
-firebase.initializeApp({
+if (!firebase.apps.length) {
+  firebase.initializeApp({
   apiKey: "AIzaSyBD1ZZ-FzcVuz2cqGpxH91OXMtZXLPV-Fw",
-  authDomain: "starry-start.firebaseapp.com"
-})
+  authDomain: "starry-start.web.app",
+  databaseURL: "https://starry-start.firebaseio.com"
+});
+} else {
+  firebase.app();
+}
 
 class App extends Component {
-  state = {
-    isSignedIn: false
+  constructor(props) {
+    super(props);
+    this.toggleMoonDisplay = this.toggleMoonDisplay.bind(this);
+    this.toggleSpaceDisplay = this.toggleSpaceDisplay.bind(this);
+
+    this.state = {
+      isSignedIn: false,
+      displayMoon: false,
+      displaySpace: false
+    }
   }
 
   uiConfig = {
@@ -20,9 +33,11 @@ class App extends Component {
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
-      signInSuccess: () => false
+      signInSuccessWithAuthResult: () => false
     }
   }
+
+  database = firebase.database();
 
   componentDidMount = () => {
 
@@ -32,26 +47,72 @@ class App extends Component {
 
   }
 
+  toggleMoonDisplay() {
+    let moonModal = document.getElementById("moonModal");
+    if (this.state.displayMoon) {
+      this.setState({displayMoon: false})
+      moonModal.style.display = "none";
+    } else {
+      this.setState({displayMoon: true})
+      moonModal.style.display = "block";
+    }
+  }
+
+  toggleSpaceDisplay() {
+    let spaceModal = document.getElementById("spaceModal");
+    if (this.state.displaySpace) {
+      this.setState({displaySpace: false})
+      spaceModal.style.display = "none";
+    } else {
+      this.setState({displaySpace: true})
+      spaceModal.style.display = "block";
+    }
+  }
+
   render() {
-  return (
-    <div className="App">
-      {this.state.isSignedIn ? (
-      <div className="body">
-        <h1>start</h1>
-        <button>view diary</button>
-        <button>constellations</button>
-        <button>moon phase</button>
-        <button>space fact</button>
-        <button onClick={() => firebase.auth().signOut()}>log out</button>
+    // moon display here
+    let moonButton = <button onClick={this.toggleMoonDisplay}>moon phase</button>;
+    let moonModal = <div id="moonModal" className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={this.toggleMoonDisplay}>&times;</span>
+          <h3>This is today's moon phase</h3>
+          <iframe className="moonframe" src="https://www.moongiant.com/phase/today/" title="the moon today"></iframe> 
+        </div>
+      </div>;
+
+    // space display here
+    let spaceButton = <button onClick={this.toggleSpaceDisplay}>cool space fact</button>;
+    let spaceModal = <div id="spaceModal" className="modal">
+        <div className="space-modal-content">
+          <span className="close" onClick={this.toggleSpaceDisplay}>&times;</span>
+          <h3>Check out this cool space fact!</h3>
+          <p>buzz lightyear woo</p> 
+        </div>
+      </div>;
+
+
+    // rendering items
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+        <div className="body">
+          <h1>start</h1>
+          <button>view diary</button>
+          <button>constellations</button>
+          {moonButton}
+          {moonModal}
+          {spaceButton}
+          {spaceModal}
+          <button onClick={() => firebase.auth().signOut()}>log out</button>
+        </div>
+        ) : (
+        <div className="body">
+          <h1>sign in to start</h1>
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
+      )}
       </div>
-      ) : (
-      <div className="body">
-        <h1>sign in to start</h1>
-        <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
-      </div>
-    )}
-    </div>
-  );
+    );
 
   }
 }
